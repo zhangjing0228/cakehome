c8119f55bba8eb9f1c9de7585c24eebc6b0ad890
张竟 <781104908@qq.com>
2020-11-07 23:14:24 +0800

    2020年11月7日 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="app">
+
+
+
+    <router-view/>
+  </div>
+</template>
+<style  scoped>
+#app{
+  width: 100%;
+  height: 100%;
+  font-size: 16px;
+  background-color: #f00;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="index">
+    主页
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name:'Index',
+};
+</script>
